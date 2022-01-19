ff219f9588ef1cf69024254cf4e59048160f1250
aysgl <aysegul.avcu@hotmail.com>
2022-01-19 22:07:55 +0300

    api test

---
+++ src/pages/api/category/index.js
@@ -0,0 +1,4 @@
+export default function handler(req, res) {
+  const url = "https://jsonplaceholder.typicode.com/users"
+  res.status(200).json(url)
+}
--- src/pages/category/[id].js
+++ src/pages/category/[id].js
@@ -4,9 +4,30 @@ import ActionButton from '../../components/product/ActionButton';
 import SlickCard from '../../components/slick/SlickCard';
 import Seperate from '../../components/stuff/Seperate';
 import Link from "next/link"
+import { useRouter } from 'next/router'
+import useSWR from 'swr'
+
+const fetcher = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    if (res.status !== 200) {
+        throw new Error(data.message)
+    }
+    return data
+}
 
 export default function index({ slug }) {
     const [toggleViewMode, setToggleViewMode] = useState(false);
+
+    const { query } = useRouter()
+    const { data, error } = useSWR(
+        () => query.id && `/api/category/${query.id}`,
+        fetcher
+    )
+
+    if (error) return <div>{error.message}</div>
+    if (!data) return <div>Loading...</div>
 
     return (
         <Container>
