c7fb7021b1e9abad11b07e979cc3824db2db9f8e
aysgl <aysegul.avcu@hotmail.com>
2022-01-21 16:57:43 +0300

    updated cat

--- src/pages/category/index.js
+++ src/pages/category/index.js
@@ -6,7 +6,7 @@ import Seperate from '../../components/stuff/Seperate'
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
 export default function Index() {
-    const { catdata, error } = useSWR('/api/category', fetcher)
+    const { data: catdata, error } = useSWR('/api/category', fetcher)
 
     if (error) return <div>Failed to load</div>
     if (!catdata) return <div>Loading cat...</div>
