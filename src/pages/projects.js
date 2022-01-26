6ee4cefdb810688b082df2156f7c0763ca96d7f2
aysgl <aysegul.avcu@hotmail.com>
2022-01-26 17:30:20 +0300

    add projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Container } from 'react-bootstrap';
+import { Card, Container, Row, Col } from 'react-bootstrap';
 import Seperate from '../components/stuff/Seperate';
 import Meta from '../components/seo/Meta';
 import { projects } from '../data/projects';
@@ -9,18 +9,25 @@ export default function Projects() {
         <>
             <Meta title="Projects" desc="desc" />
             <Container>
-                {projects.map(p =>
-                    <Card className='mb-4'>
-                        <Card.Img variant="top" src={p.img} />
-                        <Card.Body className="card-img-overlay">
-                            <div className='bg-white d-inline-block'>
-                                <Card.Title as="h3" className='fw-bold'>{p.title}</Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted">{p.created_date}</Card.Subtitle>
-                                <Card.Text>{p.body}</Card.Text>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                )}
+                <Row>
+                    {projects.map(p =>
+                        <Col md={6}>
+                            <Card className='mb-4'>
+                                <Card.Img src={p.img} />
+                                <Card.Body className="card-img-overlay">
+                                    <div className='h-100 d-flex align-items-end w-75'>
+                                        <div className='bg-white p-4'>
+                                            <Card.Title as="h3" className='fw-bold'>{p.title}</Card.Title>
+                                            <Card.Subtitle className="mb-2 text-muted">{p.created_date}</Card.Subtitle>
+                                            <Card.Text>{p.body}</Card.Text>
+                                        </div>
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )}
+                </Row>
+
                 <Seperate />
             </Container>
         </>
